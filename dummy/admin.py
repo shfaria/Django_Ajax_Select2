e3cd0d831209173f70631ab89cd32970bdc29760
Saima Hossain Faria <shfaria@Saimas-MacBook-Pro.local>
2023-08-17 14:04:53 +0600

    ajax added

--- dummy/admin.py
+++ dummy/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Dum
-# Register your models here.
+from .models import Dum, Dim
+
+
 admin.site.register(Dum)
+admin.site.register(Dim)
--- dummy/views.py
+++ dummy/views.py
@@ -3,6 +3,8 @@ from django.forms import  ModelForm
 from django import forms
 from .models import Dum, Dim
 from django.forms.widgets import SelectMultiple  
+import json
+from django.http import JsonResponse
 
 class DimForm(forms.Form):
     name = forms.CharField(max_length=50, required=False, widget=forms.Textarea(attrs={'placeholder': 'Bla bla'}))
@@ -10,8 +12,23 @@ class DimForm(forms.Form):
     dum = forms.CharField(max_length=50, required=False, widget=forms.Textarea(attrs={'placeholder': 'Bla bla'}))
     dum = forms.MultipleChoiceField(choices=dum_choices, required=False, widget=forms.SelectMultiple(attrs={'class': 'js-example-basic-multiple'}))
 
+class CreationForm(ModelForm):
+    class Meta:
+        model = Dim
+        fields = '__all__'
 
-def home(request):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['dum'].queryset = Dum.objects.none()
+
+        if 'name' in self.data:
+            self.fields['dum'].queryset = Dum.objects.all()
+        elif self.instance.pk :
+            self.fields['dum'].queryset = Dum.objects.all().filter(pk=self.instance.dum.pk)
+
+            
+
+def home0(request):
     # dums = Dum.objects.all()
     if request.method == 'POST':
         form = DimForm(request.POST)
@@ -25,5 +42,29 @@ def home(request):
 
     else:
         form = DimForm()
+    dums = ''
+    return render(request, "dummy/home.html", {'form':form, 'dums':dums})
+
+def dumView(request):
+    return render(request, "dummy/dum.html")
+
+
+def home(request):
+    form = CreationForm()
+    if request.method == 'POST':
+        form = CreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            print("form valid and saved")
+        else:
+            print("not valid")
+
+    else:
+        form = DimForm()
+
+    if request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
+        term = request.GET.get('term')
+        dums = Dum.objects.all().filter(name__icontains=term)
+        return JsonResponse(list(dums.values()), safe=False)
     dums = ''
     return render(request, "dummy/home.html", {'form':form, 'dums':dums})
